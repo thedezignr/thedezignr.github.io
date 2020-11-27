8ab33e610bac1dfb640a033e0452c4e7ed9b4c4c
Harrison <fayvorharrison101@gmail.com>
2020-11-27 13:55:07 +0100

    use new portfolio images

--- src/components/portfolio/Index.js
+++ src/components/portfolio/Index.js
@@ -65,7 +65,7 @@ const Portfolio = props => {
 
   return (
     <section id="portfolio" className={style.main__container}>
-      <div ref={footballRef} style={{ backgroundColor: "#4FFF71" }} onClick={() => { setActive(0); }} className={`${style.work__card} ${active === 0 ? style.active : ""}`}>
+      <div ref={footballRef} onClick={() => { setActive(0); }} className={`${style.work__card} ${active === 0 ? style.active : ""}`}>
         <span className={style.helper}></span>
         <img src={LoveFootball} alt="love football" />
         <div className={style.card__hover}>
@@ -78,7 +78,7 @@ const Portfolio = props => {
           <p><a href="https://www.sd.com" className={`boring__link ${style.arrow__link}`}>View case study</a></p>
         </div>
       </div>
-      <div ref={loanRef} style={{ backgroundColor: "#D3E468" }} onClick={() => { setActive(1); }} className={`${style.work__card} ${active === 1 ? style.active : ""}`}>
+      <div ref={loanRef} onClick={() => { setActive(1); }} className={`${style.work__card} ${active === 1 ? style.active : ""}`}>
         <span className={style.helper}></span>
         <img src={LoanUp} alt="loan up" />
         <div className={style.card__hover}>
@@ -90,7 +90,7 @@ const Portfolio = props => {
           <p><a href="https://www.sd.com" className={`boring__link ${style.arrow__link}`}>View case study</a></p>
         </div>
       </div>
-      <div ref={inventopalRef} style={{ backgroundColor: "#0260ED" }} onClick={() => { setActive(2); }} className={`${style.work__card} ${active === 2 ? style.active : ""}`}>
+      <div ref={inventopalRef} onClick={() => { setActive(2); }} className={`${style.work__card} ${active === 2 ? style.active : ""}`}>
         <span className={style.helper}></span>
         <img src={Inventopal} alt="inventopal" />
         <div className={style.card__hover}>
@@ -102,7 +102,7 @@ const Portfolio = props => {
           <p><a href="https://www.sd.com" className={`boring__link ${style.arrow__link}`}>View case study</a></p>
         </div>
       </div>
-      <div ref={hexcordRef} style={{ backgroundColor: "#0000FF" }} onClick={() => { setActive(3); }} className={`${style.work__card} ${active === 3 ? style.active : ""}`}>
+      <div ref={hexcordRef} onClick={() => { setActive(3); }} className={`${style.work__card} ${active === 3 ? style.active : ""}`}>
         <span className={style.helper}></span>
         <img src={Hexcord} alt="hexcord" />
         <div className={style.card__hover}>
@@ -114,31 +114,7 @@ const Portfolio = props => {
           <p><a href="https://www.sd.com" className={`boring__link ${style.arrow__link}`}>View case study</a></p>
         </div>
       </div>
-      <div ref={spineroRef} style={{ backgroundColor: "#F2F2F2" }} onClick={() => { setActive(4); }} className={`${style.work__card} ${active === 4 ? style.active : ""}`}>
-        <span className={style.helper}></span>
-        <img src={Spinero} alt="spinero" />
-        <div className={style.card__hover}>
-          <p>UI Design</p>
-          <div>
-            <h2>Spinero</h2>
-            <p>A fashion e-commerce store</p>
-          </div>
-          <p><a href="https://www.sd.com" className={`boring__link ${style.arrow__link}`}>View case study</a></p>
-        </div>
-      </div>
-      <div ref={sprintRef} style={{ backgroundColor: "#58D658" }} onClick={() => { setActive(5); }} className={`${style.work__card} ${active === 5 ? style.active : ""}`}>
-        <span className={style.helper}></span>
-        <img src={Sprinttrip} alt="sprint trip" />
-        <div className={style.card__hover}>
-          <p>UI Design</p>
-          <div>
-            <h2>Sprintrip</h2>
-            <p>A booking app</p>
-          </div>
-          <p><a href="https://www.sd.com" className={`boring__link ${style.arrow__link}`}>View case study</a></p>
-        </div>
-      </div>
-      <div ref={pizzaJungleRef} style={{ backgroundColor: "#FC5353" }} onClick={() => { setActive(6); }} className={`${style.work__card} ${active === 6 ? style.active : ""}`}>
+      <div ref={pizzaJungleRef} onClick={() => { setActive(4); }} className={`${style.work__card} ${active === 4 ? style.active : ""}`}>
         <span className={style.helper}></span>
         <img src={PizzaJungle} alt="pizza jungle" />
         <div className={style.card__hover}>
@@ -150,7 +126,7 @@ const Portfolio = props => {
           <p><a href="https://www.sd.com" className={`boring__link ${style.arrow__link}`}>View case study</a></p>
         </div>
       </div>
-      <div ref={deliverieRef} style={{ backgroundColor: "#0E87DC" }} onClick={() => { setActive(7); }} className={`${style.work__card} ${active === 7 ? style.active : ""}`}>
+      <div ref={deliverieRef} onClick={() => { setActive(5); }} className={`${style.work__card} ${active === 5 ? style.active : ""}`}>
         <span className={style.helper}></span>
         <img src={Deliverie} alt="deliverie app" />
         <div className={style.card__hover}>
@@ -158,6 +134,30 @@ const Portfolio = props => {
           <div>
             <h2>Deliverie</h2>
             <p>A delivery app</p>
+          </div>
+          <p><a href="https://www.sd.com" className={`boring__link ${style.arrow__link}`}>View case study</a></p>
+        </div>
+      </div>
+      <div ref={spineroRef} onClick={() => { setActive(6); }} className={`${style.work__card} ${active === 6 ? style.active : ""}`}>
+        <span className={style.helper}></span>
+        <img src={Spinero} alt="spinero" />
+        <div className={style.card__hover}>
+          <p>UI Design</p>
+          <div>
+            <h2>Spinero</h2>
+            <p>A fashion e-commerce store</p>
+          </div>
+          <p><a href="https://www.sd.com" className={`boring__link ${style.arrow__link}`}>View case study</a></p>
+        </div>
+      </div>
+      <div ref={sprintRef} onClick={() => { setActive(7); }} className={`${style.work__card} ${active === 7 ? style.active : ""}`}>
+        <span className={style.helper}></span>
+        <img src={Sprinttrip} alt="sprint trip" />
+        <div className={style.card__hover}>
+          <p>UI Design</p>
+          <div>
+            <h2>Sprintrip</h2>
+            <p>A booking app</p>
           </div>
           <p><a href="https://www.sd.com" className={`boring__link ${style.arrow__link}`}>View case study</a></p>
         </div>
